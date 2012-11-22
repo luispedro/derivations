20d6994cf61b5ef51c868506ccebb4d9e4faac7e
Luis Pedro Coelho <luis@luispedro.org>
2012-11-22 15:32:41 +0000

    BLD Do not rely on env variables

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/zsh
 
 function build() {
+  # This is appropriate for Ubuntu, but you might have to edit it if you get an
+  # error:
+  SAGE_ROOT=/usr/lib/sagemath/
+  TEXINPUTS="$SAGE_ROOT/local/share/texmf//:$TEXINPUTS"
   input=$1
   mkdir -p .$input.tex_files
   cd .$input.tex_files
@@ -11,5 +15,6 @@ function build() {
   cp $input.pdf ..
   cd ..
 }
+
 input=`basename $1 .tex`
 build $input
